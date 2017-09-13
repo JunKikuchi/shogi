d0b934c7533ec580ad47a0573c63a0053936db85
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-14 01:31:15 +0900

    ShogiBoard.checkmate 盤上の駒を動かした場合をざっくり実装

--- src/ShogiBoard/Square.hs
+++ src/ShogiBoard/Square.hs
@@ -2,6 +2,8 @@ module ShogiBoard.Square
   ( Square
   , File
   , Rank
+  , MoveFrom
+  , MoveTo
   ) where
 
 -- | 升目
@@ -12,3 +14,9 @@ data File = F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 deriving (Eq, Ord, Enum, 
 
 -- | 段
 data Rank = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 deriving (Eq, Ord, Enum, Bounded, Show)
+
+-- | 駒の移動元の升目
+type MoveFrom = Square
+
+-- | 駒の移動先の升目
+type MoveTo = Square
