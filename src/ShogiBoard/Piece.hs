331827cd5f8f3ec229ecf61f6744cc7f7cc576e5
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-16 13:44:45 +0900

    Piece 生成と成り実装

--- src/ShogiBoard/Piece.hs
+++ src/ShogiBoard/Piece.hs
@@ -1,23 +1,75 @@
 module ShogiBoard.Piece
   ( Piece
   , Promoted
+  , pawn
+  , lance
+  , knight
+  , silver
+  , gold
+  , bishop
+  , rook
+  , king
+  , promote
   ) where
 
 import ShogiBoard.Color
 
 -- | 駒
-data Piece = Piece Type Promoted Color deriving (Eq, Ord, Show)
+data Piece = Piece
+           { getType     :: Type
+           , getPromoted :: Promoted
+           , getColor    :: Color
+           } deriving (Eq, Ord, Show)
 
 -- | 駒の種類
 data Type = Pawn   -- 歩兵
-               | Lance  -- 香車
-               | Knight -- 桂馬
-               | Silver -- 銀将
-               | Gold   -- 金将
-               | Bishop -- 角行
-               | Rook   -- 飛車
-               | King   -- 王将
-               deriving (Eq, Ord, Enum, Bounded, Show)
+          | Lance  -- 香車
+          | Knight -- 桂馬
+          | Silver -- 銀将
+          | Gold   -- 金将
+          | Bishop -- 角行
+          | Rook   -- 飛車
+          | King   -- 王将
+          deriving (Eq, Ord, Enum, Bounded, Show)
 
 -- | 成駒
 type Promoted = Bool
+
+-- | 歩兵
+pawn :: Promoted -> Color -> Piece
+pawn = Piece Pawn
+
+-- | 香車
+lance :: Promoted -> Color -> Piece
+lance = Piece Lance
+
+-- | 桂馬
+knight :: Promoted -> Color -> Piece
+knight = Piece Knight
+
+-- | 銀将
+silver :: Promoted -> Color -> Piece
+silver = Piece Silver
+
+-- | 金将
+gold :: Color -> Piece
+gold = Piece Gold False
+
+-- | 角行
+bishop :: Promoted -> Color -> Piece
+bishop = Piece Bishop
+
+-- | 飛車
+rook :: Promoted -> Color -> Piece
+rook = Piece Rook
+
+-- | 王将
+king :: Color -> Piece
+king = Piece Gold False
+
+-- | 成る
+promote :: Piece -> Maybe Piece
+promote (Piece _ True _) = Nothing
+promote (Piece Gold _ _) = Nothing
+promote (Piece King _ _) = Nothing
+promote piece = Just piece { getPromoted = True }
