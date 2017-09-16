8f3e3bb34be5db83744c7103cf2d8d0836040575
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-16 14:10:53 +0900

    Board にコメント追加

--- src/ShogiBoard/Board.hs
+++ src/ShogiBoard/Board.hs
@@ -18,23 +18,30 @@ import ShogiBoard.Color
 -- | 将棋盤
 newtype Board = Board (Map.Map Square Piece) deriving (Eq, Show)
 
+-- | 升目と駒のリストから将棋盤作成
 fromList :: [(Square, Piece)] -> Board
 fromList = undefined
 
+-- | 王手判定
 check :: Color -> Board -> Bool
 check = undefined
 
+-- | 手番の駒リスト
 pieces :: Color -> Board -> [(Square, Piece)]
 pieces = undefined
 
+-- | 駒を動かす
 move :: MoveFrom -> MoveTo -> Color -> Board -> Maybe Board
 move = undefined
 
+-- | 持ち駒を指す
 drop :: Piece -> Square -> Color -> Board -> Maybe Board
 drop = undefined
 
+-- | 駒を動かせる升目リスト
 moves :: MoveFrom -> Color -> Board -> [MoveTo]
 moves = undefined
 
+-- | 持ち駒を指せる升目リスト
 drops :: Piece -> Color -> Board -> [Square]
 drops = undefined
