9e456ff366c9f195480f09df52ac48794b543da4
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-18 21:29:51 +0900

    Piece.drops のテスト作成

--- test/ShogiBoardTest/PieceTest.hs
+++ test/ShogiBoardTest/PieceTest.hs
@@ -2,10 +2,12 @@ module ShogiBoardTest.PieceTest (tests) where
 
 import Test.Tasty
 import qualified ShogiBoardTest.PieceTest.MovesTest      as MovesTest
+import qualified ShogiBoardTest.PieceTest.DropsTest      as DropsTest
 import qualified ShogiBoardTest.PieceTest.PromotionsTest as PromotionsTest
 
 tests :: TestTree
 tests = testGroup "Piece"
   [ MovesTest.tests
+  , DropsTest.tests
   , PromotionsTest.tests
   ]
---
+++ test/ShogiBoardTest/PieceTest/DropsTest.hs
@@ -0,0 +1,28 @@
+module ShogiBoardTest.PieceTest.DropsTest (tests) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import ShogiBoard.Piece
+import ShogiBoard.Square
+import ShogiBoard.Color
+
+tests :: TestTree
+tests = testGroup "drops"
+  [ testCase "先手歩兵" $ drops (pawn   False Black) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R2 .. R9]]
+  , testCase "後手歩兵" $ drops (pawn   False White) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R1 .. R8]]
+  , testCase "先手香車" $ drops (lance  False Black) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R2 .. R9]]
+  , testCase "後手香車" $ drops (lance  False White) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R1 .. R8]]
+  , testCase "先手桂馬" $ drops (knight False Black) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R3 .. R9]]
+  , testCase "後手桂馬" $ drops (knight False White) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R1 .. R7]]
+  , testCase "先手銀将" $ drops (silver False Black) @?= allSquares
+  , testCase "後手銀将" $ drops (silver False White) @?= allSquares
+  , testCase "先手金将" $ drops (gold         Black) @?= allSquares
+  , testCase "後手金将" $ drops (gold         White) @?= allSquares
+  , testCase "先手角行" $ drops (bishop False Black) @?= allSquares
+  , testCase "後手角行" $ drops (bishop False White) @?= allSquares
+  , testCase "先手飛車" $ drops (rook   False Black) @?= allSquares
+  , testCase "後手飛車" $ drops (rook   False White) @?= allSquares
+  ]
+
+allSquares :: [Square]
+allSquares = [(file, rank) | file <- [F1 .. F9], rank <- [R1 .. R9]]
