9992ad910c0b6f4dc992a0479e1b6cb4b0c8b413
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-18 22:28:55 +0900

    コメント追加

--- src/ShogiBoard/Stand.hs
+++ src/ShogiBoard/Stand.hs
@@ -14,17 +14,22 @@ import ShogiBoard.Color
 -- | 駒台
 newtype Stand = Stand [Piece] deriving (Eq, Show)
 
+-- | 駒のリストから駒台作成
 fromList :: [Piece] -> Stand
 fromList = Stand
 
+-- | 駒のリスト
 toList :: Stand -> [Piece]
 toList (Stand stand) = stand
 
+-- | 手番の持ち駒リスト
 pieces :: Color -> Stand -> [Piece]
 pieces color (Stand stand) = filter (\piece -> getColor piece == color) stand
 
+-- | 駒台から駒を取り除く
 take :: Piece -> Color -> Stand -> Maybe (Piece, Stand)
 take = undefined
 
+-- | 駒が駒台にあるか
 included :: Piece -> Stand -> Bool
 included piece (Stand stand) = elem piece stand
