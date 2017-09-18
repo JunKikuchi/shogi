7c5ac1da4bcb868bc94a1dfa12bc92edd36fd766
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-18 23:18:31 +0900

    Board.drops のテスト作成

--- test/ShogiBoardTest/BoardTest/DropsTest.hs
+++ test/ShogiBoardTest/BoardTest/DropsTest.hs
@@ -2,8 +2,19 @@ module ShogiBoardTest.BoardTest.DropsTest (tests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import ShogiBoard.Board
+import ShogiBoard.Piece hiding (drops)
+import ShogiBoard.Square
+import ShogiBoard.Color
 
 tests :: TestTree
 tests = testGroup "drops"
-  [ testCase "未実装" $ False @?= True
+  [ testCase "先手歩兵" $ drops (pawn False Black) (fromList []) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R2 .. R9]]
+  , testCase "後手歩兵" $ drops (pawn False White) (fromList []) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R1 .. R8]]
+
+  , testCase "先手歩兵二歩回避" $ drops (pawn False Black) (fromList [((file, R9), pawn False Black) | file <- [F1 .. F9]]) @?= []
+  , testCase "後手歩兵二歩回避" $ drops (pawn False White) (fromList [((file, R1), pawn False White) | file <- [F1 .. F9]]) @?= []
+
+  , testCase "先手歩兵と金" $ drops (pawn False Black) (fromList [((file, R9), pawn True Black) | file <- [F1 .. F9]]) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R2 .. R8]]
+  , testCase "後手歩兵と金" $ drops (pawn False White) (fromList [((file, R1), pawn True White) | file <- [F1 .. F9]]) @?= [(file, rank) | file <- [F1 .. F9], rank <- [R2 .. R8]]
   ]
