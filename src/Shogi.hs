6ab3395e905b591297392cd9adf7c75c0e9d5380
Kikuchi Jun <kikuchi@bonnou.com>
2017-09-27 01:06:58 +0900

    Shogi モジュールのデータ構造を修正

--- src/Shogi.hs
+++ src/Shogi.hs
@@ -21,47 +21,48 @@ data Shogi = Shogi
            } deriving (Eq, Show)
 
 -- | 手順リスト
-type Moves = [MoveA]
+type Moves = [Move]
 
 -- | 手順
-data MoveA = MoveA
-            { getColor    :: Color    -- 手番
-            , getMoveType :: MoveType -- 指した手
-            , getSec      :: Sec      -- 秒数
-            , getUTCTime  :: UTCTime  -- 時間
-            , getPosition :: Position -- 局面
-            , getClock    :: Clock    -- 時計
-            } deriving (Eq, Show)
+data Move = Move
+          { getColor     :: Color     -- 手番
+          , getMovePiece :: MovePiece -- 指した手
+          , getSec       :: Sec       -- 秒数
+          , getUTCTime   :: UTCTime   -- 時間
+          , getPosition  :: Position  -- 局面
+          , getClock     :: Clock     -- 時計
+          } deriving (Eq, Show)
 
 -- | 指した手
-data MoveType = Move MoveFrom MoveTo -- 駒を動かす
-              | Drop Piece    MoveTo -- 持ち駒を指す
-              | ResignA               -- 投了
-              | ImpasseA              -- 持将棋
-              | Clock                -- 時計を進める
-              deriving (Eq, Show)
+data MovePiece = MovePiece MoveFrom MoveTo -- 駒を動かす
+               | DropPiece Piece    MoveTo -- 持ち駒を指す
+               | Resign                    -- 投了
+               | Clock                     -- 時計を進める
+               -- | Impasse                   -- 持将棋
+               deriving (Eq, Show)
 
 -- | 結果
-data Result = InProgress            -- 対局中
-            | Drawn     Termination -- 引き分け
-            | Win Color Termination -- 勝負がついた
+data Result = InProgress                -- 対局中
+            | Drawn     DrawTermination -- 引き分け
+            | Win Color WinTermination  -- 勝負がついた
             deriving (Eq, Show)
 
--- | 終了状態
-data Termination = Checkmate         -- 詰み
-                 | TimeForfeit       -- 時間切れ
-                 | Resign            -- 投了
-                 | PerpetualCheck    -- 連続王手の千日手
-                 | IllegalMove       -- 不正な手を指した
-                 | RepetitionOfMoves -- 千日手 (     Drawn)
-                 | Impasse           -- 持将棋 (Win, Drawn)
-                 deriving (Eq, Show)
+-- | 引き分けの場合の終了状態
+data DrawTermination = Repetition     -- 千日手 (     Drawn)
+                     -- | Impasse        -- 持将棋 (Win, Drawn)
+                     deriving (Eq, Show)
+
+-- | 勝負がついた場合の終了状態
+data WinTermination = Checkmate      -- 詰み
+                    | TimeForfeit    -- 時間切れ
+                    | Resignation    -- 投了
+                    | PerpetualCheck -- 連続王手の千日手
+                    -- | Impasse        -- 持将棋 (Win, Drawn)
+                    -- | IllegalMove    -- 不正な手 (https://en.wikipedia.org/wiki/Shogi#Illegal_move)
+                    deriving (Eq, Show)
 
 shogi :: Position -> Moves -> Result -> Shogi
 shogi = undefined
-
-getLastPosition :: Shogi -> Position
-getLastPosition = undefined
 
 checkmate :: Color -> Shogi -> Bool
 checkmate = undefined
@@ -69,5 +70,5 @@ checkmate = undefined
 check :: Color -> Shogi -> Bool
 check = undefined
 
-move :: MoveA -> Shogi -> Maybe Shogi
+move :: MovePiece -> Shogi -> Maybe Shogi
 move = undefined
